dc1ed1638fa9735f6f80883f04781bc04545ab98
vanessa330 <vanessa.33010@gmail.com>
2022-11-04 06:46:17 +0800

    styling

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
-document.querySelector("#push").onclick = function () {
-  if (document.querySelector("#addTask input").value.length == 0) {
-    alert("Please add a task!");
-  } else {
-    document.querySelector("#allTasks").innerHTML += `
-        <div class="task">
-          <span id="taskname">
-            ${document.querySelector("#addTask input").value}
-         </span>
-          <button class="delete">Delete</button>
-        </div>
-    `;
+// Variable
 
-    let current_tasks = document.querySelectorAll(".delete");
-    for(let i = 0; i < current_tasks.length; i++) {
-        current_tasks[i].onclick = function() {
-            this.parentNode.remove();
-        }
+const addTask = document.getElementById("add-task");
+const taskContainer = document.getElementById("task-container");
+const inputTask = document.getElementById("input-task");
+
+// Event Listener for Add Button
+
+addTask.addEventListener("click", function() {
+
+    let taskItem = document.createElement("div");
+    taskItem.classList.add("task-item");
+
+    let li = document.createElement("li");
+    li.innerText = `${inputTask.value}`;
+    taskItem.append(li); // String Obj
+
+    let checkButton = document.createElement("button");
+    checkButton.innerHTML = `<i class="fa-solid fa-check"></i>`;
+    checkButton.classList.add("checkTask");
+    taskItem.appendChild(checkButton); // Node Obj
+
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+    deleteButton.classList.add("deleteTask");
+    taskItem.appendChild(deleteButton);
+
+
+    if(inputTask.value === "") {
+        alert("Please Enter a Task");
+    } else {
+        taskContainer.appendChild(taskItem);
     }
-  }
-};
+
+    checkButton.addEventListener("click", function() {
+
+        checkButton.parentElement.style.textDecoration = 
+        "line-through";
+    });
+
+    deleteButton.addEventListener("click", function(e) {
+
+        let target = e.target;
+        // trash bin        .div
+        target.parentElement.parentElement.remove();
+    });
+});
